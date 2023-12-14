48ba3a279f3206ecceac5af5881234e81cc630cf
mstasiak98 <mikistasiak98@gmail.com>
2023-12-14 13:45:28 +0100

    Add edit and remove post functionality

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,51 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+export const GET = async (req, { params }) => {
+
+    try {
+        await connectToDB();
+
+        const prompt = await Prompt.findById(params.id);
+        if(!prompt) return new Response("Prompt not found", { status: 404 })
+
+        return new Response(JSON.stringify(prompt), { status: 200 });
+
+    } catch (e) {
+        return new Response("Failed to fetch post", { status: 500 });
+    }
+}
+
+export const PATCH = async (req, { params }) => {
+    const { prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt) return new Response("Prompt not found", { status: 404 })
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
+
+    } catch (e) {
+        return new Response("Failed to update the post", { status: 500 });
+    }
+}
+
+export const DELETE = async (req, { params }) => {
+
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndDelete(params.id);
+
+        return new Response("Post deleted successfully", { status: 200 });
+
+    } catch (e) {
+        return new Response("Failed to remove the post", { status: 500 });
+    }
+}
